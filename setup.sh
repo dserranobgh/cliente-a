e5211e1be7609d0cd0613b2374079bf803d7bf04
daro <daro@Ubuntu.myguest.virtualbox.org>
2023-11-15 16:50:05 -0300

    adding fixed code

--- setup.sh
+++ setup.sh
@@ -10,8 +10,9 @@ terraform apply -auto-approve
 
 chmod 400 cliente_a
 
-ansible-playbook -i inventory.yml nginx.yml
-ansible-playbook -i inventory.yml copyapp.yml
+ansible -i inventory.yml -m ping all -vvv
+#ansible-playbook -i inventory.yml nginx.yml
+#ansible-playbook -i inventory.yml copyapp.yml
 
 ip=$(ansible-inventory -i inventory.yml --list | jq -r '.nginx.hosts[0]')
 curl "http://${ip}" --fail
