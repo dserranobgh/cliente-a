bd7d8c2d1ae012118a99b1f3003b98efe2faffac
daro <daro@Ubuntu.myguest.virtualbox.org>
2023-11-15 11:37:31 -0300

    test

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 set -eux
 
 ansible-galaxy collection install cloud.terraform
-
+mv terraform.tfstate terraform.tfstate.bkp
 
 terraform init
 terraform apply -auto-approve
+mv terraform.tfstate.bkp terraform.tfstate
 
 ansible-playbook -i inventory.yml nginx.yml
 ansible-playbook -i inventory.yml copyapp.yml
