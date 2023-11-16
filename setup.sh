679430fec82be3d035879dc66a0137632110a95d
daro <daro@Ubuntu.myguest.virtualbox.org>
2023-11-15 21:16:36 -0300

    adding fixed code

--- setup.sh
+++ setup.sh
@@ -3,6 +3,9 @@
 set -eux
 
 ansible-galaxy collection install cloud.terraform
+ansible-galaxy collection install community.general
+ansible-galaxy collection install datadog.dd
+
 
 
 terraform init
@@ -16,6 +19,7 @@ ssh -o "StrictHostKeyChecking no" -i cliente_a ubuntu@${dns_public} exit
 
 ansible-playbook -i inventory.yml nginx.yml
 ansible-playbook -i inventory.yml copyapp.yml
+ansible-playbook -i inventory.yml datadog.yml
 
 ip=$(ansible-inventory -i inventory.yml --list | jq -r '.nginx.hosts[0]')
 curl "http://${ip}" --fail
